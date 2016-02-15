ee6322e0cf6409e5befe59691fcd694f9e86b7fd
guberti <gavin.uberti@gmail.com>
2016-02-15 14:20:17 -0800

    Add an AlienAPI system
    Allows user written aliens to get their properties (energy and tech)

---
+++ ConsoleAliensGame/src/consolealiensgame/AlienAPI.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package consolealiensgame;
+
+/**
+ *
+ * @author guberti
+ */
+public class AlienAPI {
+    private AlienContainer aC;
+    
+    AlienAPI(AlienContainer aC) {
+        this.aC = aC;
+    }
+    
+    public int energy() {
+        return aC.energy;
+    }
+    
+    public int tech() {
+        return aC.tech;
+    }
+}
