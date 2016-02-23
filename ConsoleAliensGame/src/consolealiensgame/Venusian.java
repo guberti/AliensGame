3ba42bcd3268ac4c65fa9fcdc99eeefb292de06c
IunoJones <killerbirdycat@gmail.com>
2016-02-23 10:31:35 -0800

    Added Venusian Class

---
+++ ConsoleAliensGame/src/consolealiensgame/Venusian.java
@@ -0,0 +1,33 @@
+/*
+ * 
+ */
+package consolealiensgame;
+
+/**
+ *
+ * @author guberti
+ */
+public class Venusian extends Alien {
+    int currentDir;
+    
+    public Venusian() {
+        currentDir = 1;
+    }
+    
+    // Martians move left, right, left, right
+    public MoveDir getMove(AlienAPI api) {
+        currentDir *= -1;
+        return new MoveDir(currentDir, 0);
+    }
+
+    @Override
+    public int getFightPower(AlienAPI api) {
+        // Martians should fight with half the maximum amount of fight they
+        // could fight with
+        return Math.min(api.energy(), api.tech()) / 2;
+    }
+    
+    public Action getAction(AlienAPI api) {
+        return new Action(1);
+    }
+}
