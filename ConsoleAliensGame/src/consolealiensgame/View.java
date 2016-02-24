4febe777b3a7d9579d0a833f6d5915553bc088de
guberti <gavin.uberti@gmail.com>
2016-02-24 09:03:12 -0800

    Overhauled View class to make it more usable

--- ConsoleAliensGame/src/consolealiensgame/View.java
+++ ConsoleAliensGame/src/consolealiensgame/View.java
@@ -5,29 +5,77 @@
  */
 package consolealiensgame;
 
+import java.util.List;
+
 /**
  *
  * @author guberti
  */
 public class View {
     
-    public View (AlienContainer[] aCs) {
+    private final List<AlienContainer> aCs;
+    private final int bottomX;
+    private final int bottomY;
+    private final int size;
+    
+    public View (List<AlienContainer> aCs, int bottomX, int bottomY, int size) {
         this.aCs = aCs;
+        this.bottomX = bottomX;
+        this.bottomY = bottomY;
+        this.size = size;
     }
     
     public int getEnergyAtPos(int x, int y) throws CantSeeSquareException {
-        if (topX + board.length > x ||
-                topX < x ||
-                topY + board.length > y ||
-                topY < y) {
+        if (bottomX + 2 * size < x ||
+                bottomX > x ||
+                bottomY + 2 * size < y ||
+                bottomY > y) {
             throw new CantSeeSquareException();
         }
-        return board[topX - x][topY - y];
+        for (AlienContainer aC : aCs) {
+            if (aC.x == x && aC.y == y) {
+                return aC.energy;
+            }
+        }
+        return -1;
     }
     
     public boolean isAlienAtPos(int x, int y) throws CantSeeSquareException {
         return getEnergyAtPos(x, y) > -1;
     }
+    
+    public int[] getClosestAlienPos(int x, int y) throws NoVisibleAliensException {
+        if (aCs.isEmpty()) { // If there are no visible aliens
+            throw new NoVisibleAliensException();
+        }
+        
+        // Returns an array of two numbers corresponding to the x and y of the alien
+        int alienX = aCs.get(0).x;
+        int alienY = aCs.get(0).y;
+        
+        for (AlienContainer aC : aCs) {
+            if (getPointDistance(x, y, aC.x, aC.y) < 
+                    getPointDistance(x, y, alienX, alienY)) {
+                alienX = aC.x;
+                alienY = aC.y;
+            }
+        }
+        
+        int[] pos = {alienX, alienY};
+        
+        return pos;
+    }
+    
+    private int getPointDistance(int x1, int y1, int x2, int y2) {
+        // Square roots are expensive, and we only need to determine
+        // which of two distances is the greatest, so we use the distance
+        // formula but don't square root it because we can simply compare
+        // the squares of distances instead
+        
+        int distance = (int) Math.pow(Math.abs(x1 - x2), 2);
+        return distance + (int) Math.pow(Math.abs(y1 - y2), 2);
+    }
 }
 
 class CantSeeSquareException extends Exception {}
+class NoVisibleAliensException extends Exception {}
