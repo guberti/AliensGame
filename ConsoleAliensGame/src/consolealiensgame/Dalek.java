4443d386874ce4ae424e8844e7d6965b39e50fca
gunnarmein <gmein@eastsideprep.org>
2016-02-25 14:05:45 -0800

    Moving
    Built in basic random moves

--- ConsoleAliensGame/src/consolealiensgame/Dalek.java
+++ ConsoleAliensGame/src/consolealiensgame/Dalek.java
@@ -5,20 +5,43 @@
  */
 package consolealiensgame;
 
+import java.util.Random;
+
 /**
  *
  * @author gmein
  */
 public class Dalek extends Alien
 {
+    Random rand;
+    
     public Dalek()
     {
+        rand = new Random();
     }
 
     // Martians move left, right, left, right
     public MoveDir getMove(AlienAPI api)
     {
-        return new MoveDir(1, 0);
+        int move_energy;
+        
+        // don't move more than you have tech
+        move_energy = Math.min(api.energy(), api.tech());
+        // don't move more than 5, leave energy for other stuff
+        move_energy = Math.min(move_energy, 5);
+        
+        // spend a random amount of that moving into x direction
+        int x = rand.nextInt() % move_energy;
+        move_energy -= x;
+        
+        // and y takes the rest
+        int y = move_energy;
+        
+        // don't move off the board
+        x = Math.min(api.x(), x);
+        y = Math.min(api.y(), y);
+                
+        return new MoveDir(x, y);
     }
 
     public Action getAction(AlienAPI api)
