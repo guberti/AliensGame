e95ff7b1eb663720634c4e16174cbc971f999042
gunnarmein <gmein@eastsideprep.org>
2016-02-26 14:50:47 -0800

    Committed directly from netbeans. Changed a little of Dalek to have debug output.

--- StockAliens/src/stockaliens/Dalek.java
+++ StockAliens/src/stockaliens/Dalek.java
@@ -12,89 +12,75 @@ import java.util.Random;
  *
  * @author gmein
  */
-public class Dalek implements Alien
-{
+public class Dalek implements Alien {
+
     Random rand;
-    
-    public Dalek()
-    {
+    final boolean debug = true;
+
+    public Dalek() {
         rand = new Random();
+
+        if (debug) {
+            System.out.println("Dalek created");
+        }
     }
 
     // Martians move left, right, left, right
-    public MoveDir getMove(Context api)
-    {
-        // test code for JAR loader
-        if (api == null) 
-        {
-            System.out.println("Test of dynamic Dalek loader successful");
-            return null;
-        }
+    public MoveDir getMove(Context api) {
         
         int move_energy;
-        
+
         // don't move more than you have tech
         move_energy = Math.min(api.getEnergy(), api.getTech());
         // don't move more than 5, leave energy for other stuff
         move_energy = Math.min(move_energy, 5);
-        
+
         // spend a random amount of that moving into x direction
         int x = rand.nextInt() % move_energy;
         move_energy -= x;
-        
+
         // and y takes the rest
         int y = move_energy;
-        
+
         // don't move off the board
         x = Math.min(api.getX(), x);
         y = Math.min(api.getY(), y);
-                
+
         return new MoveDir(x, y);
     }
 
-    public Action getAction(Context api)
-    {
+    public Action getAction(Context api) {
         View view = api.getView();
 
         // catch and shenanigans
-        try
-        {
+        try {
             // is there another alien on our position?
-            if (view.isAlienAtPos(api.getX(), api.getY()))
-            {
+            if (view.isAlienAtPos(api.getX(), api.getY())) {
                 // if so, do we have any energy?
-                if (api.getEnergy() < 3)
-                {
+                if (api.getEnergy() < 3) {
                     // no, lie still and start praying
-                    return new Action (ActionCode.Gain);
+                    return new Action(ActionCode.Gain);
                 }
-                
+
                 // or, spend our energy on fighting
-                return new Action (ActionCode.Fight, api.getEnergy() - 2);
+                return new Action(ActionCode.Fight, api.getEnergy() - 2);
             }
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             // do something here to deal with errors
         }
 
-        
         //
         // if we have spare energy, research tech
         //
-        
-        if (api.getEnergy() > (api.getTech() + 2))
-        {
+        if (api.getEnergy() > (api.getTech() + 2)) {
             return new Action(ActionCode.Research);
         }
         return new Action(ActionCode.Gain);
     }
 
-    public void processResults(Context api)
-    {
+    public void processResults(Context api) {
         return;
     }
 
-    
 }
 
-class LoaderException extends Exception {}
